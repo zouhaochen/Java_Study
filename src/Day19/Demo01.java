6c46426c4a5169b66dfa278818569553e34ef604
Haochen Zou <zouhaochenluke@gmail.com>
2021-12-09 09:17:41 -0500

    day19

---
+++ src/Day19/Demo01.java
@@ -0,0 +1,41 @@
+package Day19;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+字符流的缓冲区
+1. 缓冲区的出现提高了对数据的读写效率
+2. 对应类:
+   BufferedWriter
+   BufferedReader
+3. 缓冲区要结合流才可以使用
+4. 在流的基础上对流的功能进行了增强
+
+缓冲区的出现是为了提高流的操作效率而出现的
+所以在创建缓冲区之前必须要先有流对象
+该缓冲区中提供了一个跨平台的换行符: newLine();
+ */
+public class Demo01
+{
+    public static void main(String[] args) throws IOException
+    {
+        //创建一个字符写入流对象
+        FileWriter fw = new FileWriter("buffer.txt");
+
+        //为了提高字符写入流的效率，加入了缓冲技术，只要将需要被提高效率的流对象作为参数传递给缓冲区的构造函数即可
+        BufferedWriter bufw = new BufferedWriter(fw);
+
+        bufw.write("abcde");
+        //换行
+        bufw.newLine();
+
+        //记住，只要用到缓冲区，就要记得刷新
+        bufw.flush();
+
+        //其实关闭缓冲区就是就是在关闭缓冲区中的流对象
+        bufw.close();
+    }
+
+}
---
+++ src/Day19/Demo03.java
@@ -0,0 +1,57 @@
+package Day19;
+
+import java.io.*;
+
+/*
+通过缓冲区读取一个.java文件
+ */
+public class Demo03
+{
+    public static void main(String[] args)
+    {
+        BufferedReader bufr = null;
+        BufferedWriter bufw = null;
+
+        try
+        {
+            bufr = new BufferedReader(new FileReader("Demo.java"));
+            bufw = new BufferedWriter(new FileWriter("DemoCopy.java"));
+
+            String line = null;
+            while((line=bufr.readLine())!=null)
+            {
+                bufw.write(line);
+                bufw.flush();
+            }
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException("读写失败");
+        }
+        finally
+        {
+            try
+            {
+                if(bufr!=null)
+                {
+                    bufr.close();
+                }
+            }
+            catch(IOException e)
+            {
+                throw new RuntimeException("读取关闭失败");
+            }
+            try
+            {
+                if(bufw!=null)
+                {
+                    bufw.close();
+                }
+            }
+            catch(IOException e)
+            {
+                throw new RuntimeException("写入关闭失败");
+            }
+        }
+    }
+}
